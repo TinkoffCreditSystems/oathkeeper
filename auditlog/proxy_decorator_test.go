6c37808df068ea9f95c327338068f4b97483ec6e
torilov <d.torilov@gmail.com>
2020-12-29 15:41:19 +0300

    test: proxy decorator

---
+++ auditlog/proxy_decorator_test.go
@@ -0,0 +1,142 @@
+package auditlog
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/ory/x/logrusx"
+)
+
+type MockProxy struct {
+	mock.Mock
+	t         *testing.T
+	roundTrip func(t *testing.T, r *http.Request) (*http.Response, error)
+	director  func(t *testing.T, r *http.Request)
+}
+
+func (m *MockProxy) RoundTrip(r *http.Request) (*http.Response, error) {
+	m.Called(r)
+	return m.roundTrip(m.t, r)
+}
+
+func (m *MockProxy) Director(r *http.Request) {
+	m.Called(r)
+	m.director(m.t, r)
+}
+
+type MockSender struct {
+	mock.Mock
+}
+
+func (m *MockSender) Send(e Event) {
+	m.Called(e)
+}
+
+func TestProxyAuditLogDecorator_Director(t *testing.T) {
+	request := &http.Request{}
+	proxy := &MockProxy{
+		director: func(t *testing.T, r *http.Request) {},
+	}
+
+	decorator := ProxyAuditLogDecorator{proxy: proxy}
+	proxy.On("Director", request).Return()
+	decorator.Director(request)
+	proxy.AssertExpectations(t)
+}
+
+func TestProxyAuditLogDecorator_RoundTrip(t *testing.T) {
+	tests := []struct {
+		request  *http.Request
+		response *http.Response
+		hasErr   bool
+		proxy    *MockProxy
+		builders []EventBuilder
+		senders  []Sender
+	}{
+		{
+			request:  &http.Request{},
+			response: &http.Response{},
+			proxy: &MockProxy{
+				roundTrip: func(t *testing.T, r *http.Request) (*http.Response, error) {
+					return &http.Response{}, nil
+				},
+			},
+			builders: []EventBuilder{},
+			senders:  []Sender{},
+		},
+		{
+			request:  &http.Request{},
+			response: &http.Response{Body: ioutil.NopCloser(bytes.NewReader([]byte("test")))},
+			proxy: &MockProxy{
+				roundTrip: func(t *testing.T, r *http.Request) (*http.Response, error) {
+					return &http.Response{Body: ioutil.NopCloser(bytes.NewReader([]byte("test")))}, nil
+				},
+			},
+			builders: []EventBuilder{},
+			senders:  []Sender{},
+		},
+	}
+
+	for _, tst := range tests {
+		decorator := ProxyAuditLogDecorator{proxy: tst.proxy}
+		tst.proxy.On("RoundTrip", tst.request).Return(tst.response, nil)
+		resp, err := decorator.RoundTrip(tst.request)
+		assert.Equal(t, resp, tst.response)
+		assert.Nil(t, err)
+		tst.proxy.AssertExpectations(t)
+	}
+}
+
+func TestProxyAuditLogDecorator_RoundTrip2(t *testing.T) {
+	request, _ := http.NewRequest("GET", "http://localhost:8080/return200", nil)
+	response := &http.Response{Body: ioutil.NopCloser(bytes.NewReader([]byte("test")))}
+	proxy := &MockProxy{
+		roundTrip: func(t *testing.T, r *http.Request) (*http.Response, error) {
+			return &http.Response{Body: ioutil.NopCloser(bytes.NewReader([]byte("test")))}, nil
+		},
+	}
+	builders := []EventBuilder{
+		{
+			URLPattern: "^http://(localhost|127.0.0.1):8080/return200$",
+			r:          regexp.MustCompile("^http://(localhost|127.0.0.1):8080/return200$"),
+			Method:     "GET",
+			Filter: Filter{
+				RequestHeaderWhiteList: []string{"User-Agent"},
+			},
+			DescriptionTemplate: "Curl GET to localhost returned {{meta.response_code}}",
+		},
+	}
+	sender := &MockSender{}
+
+	decorator := ProxyAuditLogDecorator{
+		proxy:    proxy,
+		logger:   logrusx.New("", ""),
+		builders: builders,
+		senders:  []Sender{sender},
+	}
+
+	proxy.On("RoundTrip", request).Return(response, nil)
+	sender.On("Send", Event{
+		Description:    "",
+		RequestHeader:  map[string]string{"User-Agent": ""},
+		RequestBody:    map[string]interface{}{},
+		ResponseHeader: map[string]string{},
+		ResponseBody:   map[string]interface{}{},
+		Meta: map[string]string{
+			"method":      "GET",
+			"status_code": "0",
+			"url":         "http://localhost:8080/return200",
+			"user_ip":     "",
+		}, OathkeeperError: error(nil)}).Return()
+	resp, err := decorator.RoundTrip(request)
+	assert.Equal(t, resp, response)
+	assert.Nil(t, err)
+	proxy.AssertExpectations(t)
+	sender.AssertExpectations(t)
+}
